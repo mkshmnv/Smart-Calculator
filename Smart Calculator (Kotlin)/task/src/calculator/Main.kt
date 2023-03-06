5cc7432d1e1d6e86c740b2f403d453b758b23244
Mykola <nickkosh@gmail.com>
2023-03-06 11:03:54 +0200

    done Stage 2/8
    Project: Smart Calculator (Kotlin)

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -1,6 +1,23 @@
 package calculator
 
 fun main() {
-    val input = readln().split(" ").map { it.toInt() }
-    println(input.sum())
+
+    calculate()
+
+}
+
+fun calculate() {
+    val input = readln().split(" ")
+
+    if (input[0] == "/exit") {
+        println("Bye!")
+    } else if (input[0] == "") {
+        calculate()
+    } else if (input.size == 1) {
+        println(input[0])
+        calculate()
+    } else {
+        println(input[0].toInt() + input[1].toInt())
+        calculate()
+    }
 }
