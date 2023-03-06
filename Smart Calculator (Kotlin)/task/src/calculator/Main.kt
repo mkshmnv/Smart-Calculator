8b8afe6fcf9cf549c7814d13ef05a0fa13a4151c
Mykola <nickkosh@gmail.com>
2023-03-06 18:46:22 +0200

    save
    Project: Smart Calculator (Kotlin)

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -9,15 +9,40 @@ fun main() {
 fun calculate() {
     val input = readln().split(" ")
 
-    if (input[0] == "/exit") {
-        println("Bye!")
-    } else if (input[0] == "") {
-        calculate()
-    } else if (input.size == 1) {
-        println(input[0])
-        calculate()
-    } else {
-        println(input[0].toInt() + input[1].toInt())
-        calculate()
+    when {
+        input.contains("/exit") -> {
+            println("Bye!")
+        }
+
+        input.contains("/help") -> {
+            println("The program calculates the sum of numbers")
+            calculate()
+        }
+
+        input.first() == "" -> {
+            calculate()
+        }
+
+        input.size == 1 -> {
+            println(input.first())
+            calculate()
+        }
+
+
+        else -> {
+            input.filter { it }
+            var temp = 0
+
+            for (i in input) {
+                if (i.toInt() in -9..9) {
+                    temp = i.toInt()
+                }
+            }
+
+
+
+            println(input.filter { it != " " }.sumOf { it.toInt() })
+            calculate()
+        }
     }
 }
