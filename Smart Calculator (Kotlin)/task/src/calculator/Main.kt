1f07c9e357c89236dcdf3f8d054675f6d0102407
Mykola <nickkosh@gmail.com>
2023-03-07 13:34:01 +0200

    Done Stage 5/8: Error!
    Project: Smart Calculator (Kotlin)

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -11,37 +11,46 @@ fun calculate() {
     val input = Regex(""" +""").split(readln())
 
     when {
-        input.contains("/exit") -> {
-            println("Bye!")
-        }
-
-        input.contains("/help") -> {
-            println("The program calculates the sum of numbers")
-            calculate()
-        }
-
         input.first() == "" -> {
             calculate()
         }
 
-        input.size == 1 -> {
-            println(input.first())
-            calculate()
+        input.first().toIntOrNull() != null  -> {
+            if (input.size == 1) {
+                println(input.first().toInt())
+                calculate()
+            } else {
+                var output = input.first().toInt()
+                for ((index, value) in input.withIndex()) {
+                    if (index != 0 && index != input.lastIndex) {
+                        if (value.contains("+") || (value.contains("-") && value.length % 2 == 0)) {
+                            output += input[index + 1].toInt()
+                        } else if (value.contains("-")) {
+                            output -= input[index + 1].toInt()
+                        }
+                    }
+                }
+                println(output)
+                calculate()
+            }
         }
 
         else -> {
-            var output = input.first().toInt()
-            for ((index, value) in input.withIndex()) {
-                if (index != 0 && index != input.lastIndex) {
-                    if (value.contains("+") || (value.contains("-") && value.length % 2 == 0)) {
-                        output += input[index + 1].toInt()
-                    } else if (value.contains("-")) {
-                        output -= input[index + 1].toInt()
-                    }
+            if (input.first().contains("/")) {
+                if (input.contains("/exit")) {
+                    println("Bye!")
+                } else if (input.contains("/help")) {
+                    println("The program calculates the sum of numbers")
+                    calculate()
+                } else {
+                    println("Unknown command")
+                    calculate()
                 }
+            } else {
+                println("Invalid expression")
+                calculate()
             }
-            println(output)
-            calculate()
         }
     }
 }
+
